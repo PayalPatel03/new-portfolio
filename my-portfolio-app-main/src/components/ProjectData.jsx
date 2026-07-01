aa338cf13e4aca0b6c02aa515b799b17e01c7973
PayalPatel03 <p6579113@gmail.com>
2026-07-01 11:47:06 +0530

    d

--- my-portfolio-app-main/src/components/ProjectData.jsx
+++ my-portfolio-app-main/src/components/ProjectData.jsx
@@ -1,53 +1,84 @@
-import nova from "../assets/images/projects/Nova-SkinCare.png";
-import netflixClone from "../assets/images/projects/netflix-clone.png";
-import foodApp from "../assets/images/projects/food-app.png"
-import t12 from "../assets/images/projects/Animation-web.png";
-import MiniBlog from "../assets/images/projects/MiniBlog.png";
+
 import Marketing from "../assets/images/projects/marketing-solution.png";
+import Fitness from "../assets/images/projects/fitness.png";
+import Product from "../assets/images/projects/product-management.png";
+import Blog from "../assets/images/projects/blog-project.png";
+import Croma from "../assets/images/projects/croma.png";
+
 
 const ProjectData = [
-  {
-    id: 1,
-    name: "Nova Skincare",
-    image: nova,
-    link: "https://project-js-add-to-cart.vercel.app/",
-    github: "https://github.com/Tisapatel/PROJECT-JS_AddToCart",
-  },
-  {
-    id: 2,
-    name: "Netflix Clone",
-    image: netflixClone,
-    link: "https://netflix-clone-react-beta-two.vercel.app/login",
-    github: "https://github.com/Tisapatel/netflix-clone-react",
-  },
-  {
-    id: 3,
-    name: "food App",
-    image: foodApp,
-    link: "https://food-app-one-theta.vercel.app/",
-    github: "https://github.com/Tisapatel/food-app",
-  },
-  {
-    id: 3,
-    name: "Animation Web",
-    image: t12,
-    link: "https://t12-project.vercel.app/",
-    github: "https://github.com/Tisapatel/t12-project",
-  },
+  // {
+  //   id: 1,
+  //   name: "Nova Skincare",
+  //   image: nova,
+  //   link: "https://project-js-add-to-cart.vercel.app/",
+  //   github: "https://github.com/Tisapatel/PROJECT-JS_AddToCart",
+  // },
+  // {
+  //   id: 2,
+  //   name: "Netflix Clone",
+  //   image: netflixClone,
+  //   link: "https://netflix-clone-react-beta-two.vercel.app/login",
+  //   github: "https://github.com/Tisapatel/netflix-clone-react",
+  // },
+  // {
+  //   id: 3,
+  //   name: "food App",
+  //   image: foodApp,
+  //   link: "https://food-app-one-theta.vercel.app/",
+  //   github: "https://github.com/Tisapatel/food-app",
+  // },
+  // {
+  //   id: 3,
+  //   name: "Animation Web",
+  //   image: t12,
+  //   link: "https://t12-project.vercel.app/",
+  //   github: "https://github.com/Tisapatel/t12-project",
+  // },
+  // {
+  //   id: 4,
+  //   name: "Mini Blog",
+  //   image: MiniBlog,
+  //   link: "https://pr-login-system-with-blog-project.vercel.app/login",
+  //   github: "https://github.com/Tisapatel/pr-login-system-with-blog-project",
+  // },
+  
   {
     id: 4,
-    name: "Mini Blog",
-    image: MiniBlog,
-    link: "https://pr-login-system-with-blog-project.vercel.app/login",
-    github: "https://github.com/Tisapatel/pr-login-system-with-blog-project",
+    name: "Fitness",
+    image: Fitness,
+    link: "https://fitness-bootstrap.vercel.app/",
+    github: "https://github.com/PayalPatel03/fitness-bootstrap",
   },
   {
     id: 5,
     name: "Marketing Solution",
     image: Marketing,
     link: "https://66cc94a6979bd7191559f74b--steady-bavarois-93ea3d.netlify.app/",
-    github: "https://github.com/Tisapatel/CSS-PROJECT",
+    github: "https://github.com/PayalPatel03/bt-project",
   },
+  {
+    id:6,
+    name: "Product Management",
+    image: Product,
+    link: "https://product-management-mocha-rho.vercel.app/",
+    github: "https://github.com/PayalPatel03/product-management",
+  },
+   {
+    id:7,
+    name: "Blog Project",
+    image: Blog,
+    link: "https://github.com/PayalPatel03/blog-passport-project",
+    github: "https://github.com/PayalPatel03/blog-passport-project",
+  },
+   {
+    id:7,
+    name: "Croma Clone",
+    image: Croma,
+    link: "https://croma-alpha.vercel.app/",
+    github: "https://github.com/PayalPatel03/croma",
+  },
+  
 ];
 
 export default ProjectData;
